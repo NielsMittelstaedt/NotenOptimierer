7c9d5e3157bd88915b8fb4b481f98ffd924a8644
niels2000 <niels2000@hotmail.de>
2021-04-24 13:56:56 +0200

    basic json post

---
+++ NotenOptimierer/app/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class SubjectForm(forms.Form):
+    progra = forms.DecimalField()
+    
--- NotenOptimierer/app/views.py
+++ NotenOptimierer/app/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpRequest
+from django.http import JsonResponse
 from django.template import loader
-from app.solver import Solver
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .solver import Solver
+from .forms import SubjectForm
 
+@csrf_exempt
 def index(request):
-    solver = Solver("test")
-    solver.setupSolver()
-    solver.solve()
-    return render(request, 'app/index.html', {'name': "niklas"})
+    if request.method == 'POST':
+        json_data = json.loads(request.body)
+        
+        print(json_data["progra"])
+
+        data={
+            'name': 'Nikals'
+        }
+        return JsonResponse(data)
+
+    form = SubjectForm()
+    return render(request, 'app/form.html', {'form': form})
