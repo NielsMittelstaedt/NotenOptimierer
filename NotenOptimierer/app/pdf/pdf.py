ee32b7f6fb49d8227a5e1e8aed250792535df8d9
JakobMiesner <jakob.miesner@gmail.com>
2021-04-29 11:59:12 +0200

    added not working throughcut calculator

--- NotenOptimierer/app/pdf/pdf.py
+++ NotenOptimierer/app/pdf/pdf.py
@@ -5,14 +5,19 @@ import tabula
 Put your grades into a file noten.pdf and save the file into the same folder of this pdf.py
 '''
 
-class Modul():
-    def __init__(self, name, cp, grade) -> None:
+
+class Module():
+    def __init__(self, name, grade, cp) -> None:
         self.name = name
-        self.cp = cp
-        self.grade = grade
+        self.cp = float(cp.replace(",", "."))
+        try:
+            self.grade = float(grade.replace(",", "."))
+            self.graded = True
+        except:
+            self.graded = False
 
     def __str__(self) -> str:
-        return f'{self.name}-{self.grade}-{self.cp}'
+        return f'{self.name}-{self.grade if self.graded  else "Bestanden"}-{self.cp}'
 
     def __repr__(self) -> str:
         return self.__str__()
@@ -24,8 +29,24 @@ table = df[0]
 
 modules = []
 
-for module  in table.values:
-    if module[2] == "BE" : 
-        modules.append(Modul(module[0], module[1], module[4]))
+for module in table.values:
+    if module[2] == "BE":
+        modules.append(Module(module[0], module[1], module[4]))
 
 print(modules)
+
+
+gradedModules = [module for module in modules if module.graded == True]
+
+
+def calcThroughCutNoten(modules) -> float:
+    cp = 0
+    grade = 0
+    for module in modules:
+        cp += module.cp
+        grade += module.grade * module.cp
+    return grade / cp
+
+
+throughCut = calcThroughCutNoten(gradedModules)
+print(throughCut)
